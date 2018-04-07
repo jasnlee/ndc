0db93d8b4f1a0e313e45c9161349ec9f053c7609
Jason <jasnlee@gmail.com>
2018-04-06 22:12:36 -0500

    Adding index.php

---
+++ public_html/index.php
@@ -0,0 +1,65 @@
+<? include('inc-top.php');?>
+    
+    <div class="section no-pad-bot" id="index-banner">
+        <div class="container">
+            <br><br>
+            <h1 class="header center ndc-green-text">Welcome to National Diversity Council!</h1>
+            <div class="row center">
+                <h5 class="header col s12 light">You've joined a non-profit diverse, energizing organization uniting more than 20 staff members that cover over 25 states and nations. We've put this website together to share everything we think you might need or want to know about life within your new organization.</h5>
+            </div>
+            <div class="row center">
+                <a href="before-you-start.php" id="download-button" class="btn-large waves-effect waves-light ndc">Get Started</a>
+            </div>
+            <br><br>
+        </div>
+    </div>
+    
+    
+    <div class="container">
+        <div class="section">
+            
+            <!--   Icon Section   -->
+            <div class="row">
+                <div class="col s12 m6 l3">
+                    <div class="icon-block">
+                        <h2 class="center ndc-blue-text"><i class="material-icons">accessibility</i></h2>
+                        <h5 class="center">Benefits</h5>
+
+                        <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique elit ex, ut dignissim dui mattis ac. Nulla maximus eu tortor sed tincidunt. Sed fermentum enim vitae tortor pulvinar, eu fermentum erat hendrerit. Mauris ultricies lacinia neque vitae luctus. Vestibulum non felis libero.</p>
+                    </div>
+                </div>
+
+                <div class="col s12 m6 l3">
+                    <div class="icon-block">
+                        <h2 class="center ndc-blue-text"><i class="material-icons">attach_money</i></h2>
+                        <h5 class="center">Perks</h5>
+
+                        <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique elit ex, ut dignissim dui mattis ac. Nulla maximus eu tortor sed tincidunt. Sed fermentum enim vitae tortor pulvinar, eu fermentum erat hendrerit. Mauris ultricies lacinia neque vitae luctus. Vestibulum non felis libero.</p>
+                    </div>
+                </div>
+
+                <div class="col s12 m6 l3">
+                    <div class="icon-block">
+                        <h2 class="center ndc-blue-text"><i class="material-icons">get_app</i></h2>
+                        <h5 class="center">NDC App</h5>
+
+                        <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique elit ex, ut dignissim dui mattis ac. Nulla maximus eu tortor sed tincidunt. Sed fermentum enim vitae tortor pulvinar, eu fermentum erat hendrerit. Mauris ultricies lacinia neque vitae luctus. Vestibulum non felis libero.</p>
+                    </div>
+                </div>
+
+                <div class="col s12 m6 l3">
+                    <div class="icon-block">
+                        <h2 class="center ndc-blue-text"><i class="material-icons">search</i></h2>
+                        <h5 class="center">Find Your Way</h5>
+
+                        <p class="light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique elit ex, ut dignissim dui mattis ac. Nulla maximus eu tortor sed tincidunt. Sed fermentum enim vitae tortor pulvinar, eu fermentum erat hendrerit. Mauris ultricies lacinia neque vitae luctus. Vestibulum non felis libero.</p>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+        <br><br>
+    </div>
+    
+    
+<? include('inc-footer.php');?>
